c4ade28726b3b84f5a74c4edd88fe014aeac347b
hrcarryu <495958672@qq.com>
2021-11-10 23:36:32 +0800

    1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,3 @@
-# import pandas as pd
-# print("heelo, world")
-# df = pd.read_excel(r"record.xlsx",header=[0,1])
-# print(df)
-
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
