48b68f427284d069c2846101ecda3df5f868f518
hrcarryu <495958672@qq.com>
2021-11-10 11:51:36 +0800

    初始化

---
+++ records_save.py
@@ -0,0 +1,18 @@
+'''
+Author: your name
+Date: 2021-11-09 23:02:26
+LastEditTime: 2021-11-09 23:34:47
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: \records_save\records_save.py
+'''
+# import pandas as pd
+# print("heelo, world")
+# df = pd.read_excel(r"record.xlsx",header=[0,1])
+# print(df)
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
