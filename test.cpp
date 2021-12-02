9f7dcbbbf6a09d71d6b0e3eb138fe0e5fb60708b
hrcarryu <495958672@qq.com>
2021-12-02 16:23:32 +0800

    env

--- test.cpp
+++ test.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main() {
+    vector<int> vec(5, 1);
+    for(int i : vec) {
+        cout<<i<<endl;
+    }
     cout<<"hello, world!!"<<endl;
 }
